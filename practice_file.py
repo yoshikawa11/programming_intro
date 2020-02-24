627fc1ecd7b8b1d2e25307b23f69c6dac864501f
yoshikawa masashi <gunterrote@yahoo.co.jp>
2020-02-24 17:53:01 +0900

    p18指練習解答

--- practice_file.py
+++ practice_file.py
@@ -1,2 +1,22 @@
-print("hello world")
+#p.18指練習
+#x,y,z をリストに格納
+list = [14, 30, 7]
+list_odd = []
+list_count = len(list)
+count_even = 0
+for i in list:
+    if i % 2 == 0:
+        count_even += 1
+        continue
+    else:
+        list_odd.append(i)
+    continue
+if count_even == list_count:
+    print("所与の変数に奇数は存在しない")
+else:
+    print("最も大きい奇数:" + str(list_odd))
+
+
+
+
 
