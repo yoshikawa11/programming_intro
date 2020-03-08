9b37cf2d452669bee65cda983f5f7e8bf4bc8808
yoshikawa masashi <gunterrote@yahoo.co.jp>
2020-03-08 22:40:42 +0900

    p41指練習解答

---
+++ ch4/practice_4.py
@@ -0,0 +1,18 @@
+# p41指練習
+# ２つの文字列を引数とし、どちらか一方の文字列に他方の文字列が含まれる場合trueを、
+# それ以外の場合はfalseを返す関数
+
+
+def isIn(str1, str2):
+
+    if len(str1) > len(str2):
+        str1, str2 = str2, str1
+    if str1 in str2:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print(isIn('testabc', 'test'))
+    print(isIn('intro', 'programmingintro'))
